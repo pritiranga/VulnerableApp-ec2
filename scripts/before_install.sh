2f894e380248bbe6d906e8c5227ea82dec5191c0
Priti <pritiranga9@gmail.com>
2024-07-27 09:22:01 +0000

    Edited before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,1 +1,10 @@
-sfd
+#!/bin/bash
+echo "Before Install Script"
+
+# Stop the existing application server if it is running
+if pgrep -f 'java -jar my-java-app-artifact.jar'; then
+  pkill -f 'java -jar my-java-app-artifact.jar'
+fi
+
+# Create the destination directory if it doesn't exist
+mkdir -p /opt/javaapp
