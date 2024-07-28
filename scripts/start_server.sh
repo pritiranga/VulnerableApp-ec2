c2760b18f79bc7ecce89e2237062068fe980884f
pritiranga <pritiranga9@gmail.com>
2024-07-28 11:28:53 +0530

    update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Starting the server"
 
+# Create the log directory if it doesn't exist
+mkdir -p /var/log/javaapp
+
 # Start the application in the background and redirect output to a log file
 nohup java -jar /opt/javaapp/VulnerableApp-1.0.0.jar > /var/log/javaapp/javaapp.log 2>&1 &
