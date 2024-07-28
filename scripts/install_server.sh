9b5f3024c9118b56b8e8f06bcaedabd2a5eb3595
dfsdf <fds>
2024-07-28 13:05:48 +0000

    Edited install_server.sh

--- scripts/install_server.sh
+++ scripts/install_server.sh
@@ -2,4 +2,4 @@
 echo "Installing the application"
 
 # Ensure the JAR file has the correct permissions
-chmod +x /tmp/deployment-archive/build/libs/VulnerableApp-1.0.0.jar
+chmod +x /opt/vulnerableapp/VulnerableApp-1.0.0.jar
