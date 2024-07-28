e778575c21915c2b7995f7b38cc292c43e4980e1
pritiranga <pritiranga9@gmail.com>
2024-07-28 21:41:06 +0530

    update stopserver

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Stopping the server"
 
-# Find and kill the process running the Java application
-pkill -f 'java -jar /opt/vulnerableapp/VulnerableApp-1.0.0.jar'
+# Check if the Java application is running and then stop it
+if pgrep -f 'java -jar /opt/vulnerableapp/VulnerableApp-1.0.0.jar'; then
+    pkill -f 'java -jar /opt/vulnerableapp/VulnerableApp-1.0.0.jar'
+    echo "Server stopped successfully"
+else
+    echo "Server is not running"
+fi
