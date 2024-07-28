781131df67608854c3201b90b9450fc46b207197
pritiranga <pritiranga9@gmail.com>
2024-07-28 21:11:11 +0530

    update install_server

--- scripts/install_server.sh
+++ scripts/install_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Installing the application"
-
+# If the JAR file already exists, remove it
+if [ -f /opt/codedeploy-agent/deployment-root/<deployment-id>/deployment-archive/build/libs/VulnerableApp-1.0.0.jar ]; then
+  rm /opt/codedeploy-agent/deployment-root/<deployment-id>/deployment-archive/build/libs/VulnerableApp-1.0.0.jar
+fi
 # Ensure the JAR file has the correct permissions
 chmod +x /opt/codedeploy-agent/deployment-root/<deployment-id>/deployment-archive/build/libs/VulnerableApp-1.0.0.jar
