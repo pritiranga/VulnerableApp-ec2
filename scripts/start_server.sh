a39783d408eb92e94882d5412bf8bfdee929bc93
Priti <pritiranga9@gmail.com>
2024-07-28 13:06:36 +0000

    Edited start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,4 +5,4 @@ echo "Starting application"
 mkdir -p /var/log/vulnerableapp
 
 # Start the application in the background and redirect output to a log file
-nohup java -jar /tmp/deployment-archive/build/libs/VulnerableApp-1.0.0.jar > /var/log/vulnerableapp/application.log 2>&1 &
+nohup java -jar /opt/vulnerableapp/VulnerableApp-1.0.0.jar > /var/log/vulnerableapp/application.log 2>&1 &
