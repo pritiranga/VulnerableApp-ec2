934bfa245d6cc97d5b646ef96f725a53d13d5d2d
jkdskj <df>
2024-07-28 06:09:31 +0000

    Edited install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,3 @@ echo "Installing dependencies"
 # Update package list and install Java (if not already installed)
 apt-get update
 apt-get install -y openjdk-11-jdk
-
-# Optionally, install other dependencies like database clients, monitoring tools, etc.
-# apt-get install -y some-other-dependency
